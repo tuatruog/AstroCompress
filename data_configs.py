a1010fb0d054af9957b034703bd3682f396a6253
tuatruog <tuannt2@uci.edu>
2024-08-27 01:59:58 -0700

    Add data configs

---
+++ data_configs.py
@@ -0,0 +1,10 @@
+# Configurations for data loading.
+# Update this according to your own setup; no need to commit local changes.
+
+
+# Change to your personal local hugging face cache directory
+DATA_CACHE_DIR = '/extra/ucibdl1/shared/data/astrocomp/.cache'
+# Use 'tiny' for tiny dataset for fast experimenting, change to 'full' for real training
+DATA_PROFILE = 'full'
+# Where local data is stored:
+LOCAL_DATA_ROOT = '/extra/ucibdl1/shared/data/astrocomp'
